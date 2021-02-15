d13502734c3291de3c56459dd64d8464c8ecef9e
Zack Xu <zackxu419@outlook.com>
2021-02-15 15:42:07 +0800

    Removed files from statics

--- app.py
+++ app.py
@@ -17,10 +17,12 @@ color = datetime.now().hour
 
 
 @app.route('/')
-def init():
+def index():
     return render_template("index.html")
 
-
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
