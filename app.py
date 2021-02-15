79b2ca0b894e2fec34bc38089b2e944af40bfc87
Zack Xu <zackxu419@outlook.com>
2021-02-15 18:52:53 +0800

    Added DB and finished the backend (hopefully there's no errors, but i can't test until the frontend is done)

--- app.py
+++ app.py
@@ -20,6 +20,27 @@ color = datetime.now().hour
 def index():
     return render_template("index.html")
 
+def sqliteQuery(query):
+    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
+    Cursor = dbConnection.cursor()
+    Cursor.execute(query)
+    dbConnection.commit()
+
+def sqliteGet(query):
+    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
+    Cursor = dbConnection.cursor()
+    result = Cursor.execute(query)
+    result = result.fetchall()
+    return result
+
+def init():
+    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
+    initCursor = dbConnection.cursor()
+    initCursor.execute(
+        "CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, points INT, status BOOL)")
+    dbConnection.commit()
+    return True
+
 @app.route('/signup', methods = ["GET", "POST"])
 def signup():
     if request.method == "GET":
@@ -27,14 +48,36 @@ def signup():
     else:
         username = request.form["username"]
         password = request.form["password"]
+        arguments = [username, password]
+        query = "INSERT INTO users(username, password, points) VALUES ({user}, {password}, {points})".format(
+            user=arguments[0], password=arguments[1], points=0)
+        print(query)
+        sqliteArgument(query=query)
+
+@app.route('/init')
+def init():
+    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
+    initCursor = dbConnection.cursor()
+    initCursor.execute(
+        "CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, points INT, status BOOL)")
+    dbConnection.commit()
+    return "done"
+
 
 @app.route('/signin', methods = ["GET", "POST"])
-def signup():
+def signin():
     if request.method == "GET":
         return render_template("signin.html")
     else:
         username = request.form["username"]
         password = request.form["password"]
+        query = "SELECT * FROM usere WHERE username = {username}".format(username=username)
+        result = sqliteGet(query)
+        if not result:
+            return "Username doesn't exist"
+        if result[0][1] == password:
+            return "Logged In!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
