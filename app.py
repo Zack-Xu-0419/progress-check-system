e0ccffabb43de4510d73e33a566acdcd9ec3d1b5
TheFightingCatfish <seekingblues@gmail.com>
2021-02-17 00:33:42 +0800

    Added files and clean up

--- app.py
+++ app.py
@@ -1,83 +1,60 @@
-import sqlite3
 import os
-import datetime
-import time
-from datetime import datetime, timedelta
-from pathlib import Path
-
+import sqlite3
 from flask import Flask, request, url_for, render_template, redirect, session
 
 currentDirectory = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
-
 app.secret_key = "Hspt"
 
-color = datetime.now().hour
+
+def sqliteQuery(*query):
+    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
+    cursor = dbConnection.cursor()
+    cursor.execute(*query)
+    dbConnection.commit()
 
 
-@app.route('/')
+def sqliteGet(*query):
+    cursor = sqlite3.connect(currentDirectory + "/peerCheckSystem.db").cursor()
+    result = cursor.execute(*query).fetchall()
+    return result
+
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-def sqliteQuery(query):
-    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
-    Cursor = dbConnection.cursor()
-    Cursor.execute(query)
-    dbConnection.commit()
 
-def sqliteGet(query):
-    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
-    Cursor = dbConnection.cursor()
-    result = Cursor.execute(query)
-    result = result.fetchall()
-    return result
-
+@app.route("/init")
 def init():
-    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
-    initCursor = dbConnection.cursor()
-    initCursor.execute(
-        "CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, points INT, status BOOL)")
-    dbConnection.commit()
-    return True
+    sqliteQuery("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, points INT, status BOOL)")
+    return "done"
 
-@app.route('/signup', methods = ["GET", "POST"])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "GET":
         return render_template("signup.html")
     else:
-        username = request.form["username"]
-        password = request.form["password"]
-        arguments = [username, password]
-        query = "INSERT INTO users(username, password, points) VALUES ({user}, {password}, {points})".format(
-            user=arguments[0], password=arguments[1], points=0)
-        print(query)
-        sqliteArgument(query=query)
-
-@app.route('/init')
-def init():
-    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
-    initCursor = dbConnection.cursor()
-    initCursor.execute(
-        "CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, points INT, status BOOL)")
-    dbConnection.commit()
-    return "done"
+        sqliteQuery("INSERT INTO users (username, password, points) VALUES (?, ?, 0)", (request.form["username"], request.form["password"]))
+        return "done"
+#   TODO Detects if username is already taken.
 
 
-@app.route('/signin', methods = ["GET", "POST"])
+@app.route("/signin", methods=["GET", "POST"])
 def signin():
     if request.method == "GET":
         return render_template("signin.html")
     else:
-        username = request.form["username"]
-        password = request.form["password"]
-        query = "SELECT * FROM usere WHERE username = {username}".format(username=username)
-        result = sqliteGet(query)
+        result = sqliteGet("SELECT * FROM users WHERE username = ?", (request.form["username"],))
         if not result:
             return "Username doesn't exist"
-        if result[0][1] == password:
+        elif result[0][1] != request.form["password"]:
+            return "Incorrect password"
+        else:
             return "Logged In!"
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
