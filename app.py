a66818e76809232d709fe64e866584011be2c0b0
TheFightingCatfish <seekingblues@gmail.com>
2021-06-16 17:01:04 +0800

    Major update

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
 import os
-import ast
+import crypt
 import sqlite3
 from functools import wraps
+from ast import literal_eval
+from hmac import compare_digest
 from flask import Flask, request, url_for, render_template, redirect, session, json
 
-currentDirectory = os.path.dirname(os.path.abspath(__file__))
-
-app = Flask(__name__)
-app.secret_key = b"\xa3\x08\x94\xa2ED\x10\xa4@:6\xb6=i\xe8\xec"
+DB = "./database.db"
 
 
-def sqliteQuery(*query):
-    dbConnection = sqlite3.connect(currentDirectory + "/peerCheckSystem.db")
-    cursor = dbConnection.cursor()
+def sqlite_execute(*query):
+    conn = sqlite3.connect(DB)
+    cursor = conn.cursor()
     cursor.execute(*query)
-    dbConnection.commit()
+    conn.commit()
 
 
-def sqliteGet(*query):
-    cursor = sqlite3.connect(currentDirectory + "/peerCheckSystem.db").cursor()
-    result = cursor.execute(*query).fetchall()
-    return result
+def sqlite_get(*query):
+    cursor = sqlite3.connect(DB).cursor()
+    return cursor.execute(*query).fetchall()
 
 
 def signin_required(signed_in):
     def dec(f):
         @wraps(f)
         def wrapper():
-            if int(signed_in) + int("user" in session) == 1:
+            if signed_in is not ("user" in session):
                 return redirect(url_for("index"))
             return f()
         return wrapper
@@ -47,6 +45,20 @@ def search_logout(f):
     return wrapper
 
 
+def get_groups():
+    groups = sqlite_get("SELECT name, members FROM groups WHERE members LIKE ?", ("%'" + session["user"] + "'%",))
+    return [(group[0], literal_eval(group[1])) for group in groups]
+
+
+app = Flask(__name__)
+app.secret_key = b"\xa3\x08\x94\xa2ED\x10\xa4@:6\xb6=i\xe8\xec"
+
+if not os.path.exists(DB):
+    os.mknod(DB)
+
+sqlite_execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, points INT, private BOOL); CREATE TABLE IF NOT EXISTS groups (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT, members TEXT, pending TEXT)")
+
+
 @app.route("/", methods=["GET", "POST"])
 @search_logout
 def index():
@@ -55,20 +67,15 @@ def index():
     return render_template("index.html", message="Sign in or sign up")
 
 
-@app.route("/init")
-def init():
-    sqliteQuery("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, points INT, private BOOL); CREATE TABLE IF NOT EXISTS groups (name TEXT, password TEXT, members TEXT, pending TEXT)")
-    return "Done"
-
-
 @app.route("/signup", methods=["GET", "POST"])
 @signin_required(False)
 def signup():
     if request.method == "GET":
-        return render_template("signup.html", background=True)
-    result = sqliteGet("SELECT username FROM users WHERE username = ?", (request.json["username"],))
+        return render_template("signup.html")
+    result = sqlite_get("SELECT username FROM users WHERE username = ?", (request.json["username"],))
     if not result:
-        sqliteQuery("INSERT INTO users (username, password, points, private) VALUES (?, ?, 0, 0)", (request.json["username"], request.json["password"]))
+        crypt_pwd = crypt.crypt(request.json["password"], crypt.METHOD_SHA512)
+        sqlite_execute("INSERT INTO users (username, password, points, private) VALUES (?, ?, 0, 0)", (request.json["username"], crypt_pwd))
         return json.dumps({"status": 0})
     return json.dumps({"status": 1})
 
@@ -77,15 +84,15 @@ def signup():
 @signin_required(False)
 def signin():
     if request.method == "GET":
-        return render_template("signin.html", background=True)
-    result = sqliteGet("SELECT username, password FROM users WHERE username = ?", (request.json["username"],))
+        return render_template("signin.html")
+    result = sqlite_get("SELECT username, password FROM users WHERE username = ?", (request.json["username"],))
     if not result:
         return json.dumps({"status": 1})
-    if result[0][1] != request.json["password"]:
+    crypt_pwd = result[0][1]
+    pwd = request.json["password"]
+    if not compare_digest(crypt.crypt(pwd, crypt_pwd), crypt_pwd):
         return json.dumps({"status": 2})
     session["user"] = result[0][0]
-    groups = sqliteGet("SELECT name, members FROM groups WHERE members LIKE ?", ("%'" + session["user"] + "'%",))
-    session["groups"] = [(group[0], ast.literal_eval(group[1])) for group in groups]
     return json.dumps({"status": 0})
 
 
@@ -93,87 +100,104 @@ def signin():
 @signin_required(True)
 @search_logout
 def settings():
+    # GET
     if request.method == "GET":
-        privateStatus = sqliteGet("SELECT private FROM users WHERE username = ?", (session["user"],))
-        return render_template("settings.html", privateStatus=privateStatus[0][0])
-    if request.method == "POST" and request.form["button"] == "update":
-        privateStatus = bool(request.form.get("private"))
-        sqliteQuery("UPDATE users SET private = ? WHERE username = ?", (privateStatus, session["user"]))
-        return render_template("settings.html", privateStatus=privateStatus, message="Update successful")
-    sqliteQuery("DELETE FROM users WHERE username = ?", (session["user"],))
-    session.clear()
-    return redirect(url_for("index"))
+        private = sqlite_get("SELECT private FROM users WHERE username = ?", (session["user"],))[0][0]
+        return render_template("settings.html", private=private)
+
+    # Update private status
+    if request.method == "POST" and request.form.get("button") == "update":
+        private = bool(request.form.get("private"))
+        sqlite_execute("UPDATE users SET private = ? WHERE username = ?", (private, session["user"]))
+        return render_template("settings.html", private=private, message="Update successful")
+
+    # Delete account
+    if request.method == "POST" and request.form.get("button") == "delete":
+        sqlite_execute("DELETE FROM users WHERE username = ?", (session["user"],))
+        session.clear()
+        return redirect(url_for("index"))
+
+    # Update password
+    crypt_pwd = sqlite_get("SELECT password FROM users WHERE username = ?", (session["user"],))[0][0]
+    if not compare_digest(crypt.crypt(request.json["old"], crypt_pwd), crypt_pwd):
+        return json.dumps({"status": 1})
+    sqlite_execute("UPDATE users SET password = ? WHERE username = ?", (crypt.crypt(request.json["new"], crypt.METHOD_SHA512), session["user"]))
+    return json.dumps({"status": 0})
 
 
 @app.route("/search", methods=["GET", "POST"])
 @signin_required(True)
 @search_logout
 def search():
+    # GET
     if request.method == "GET":
         query = request.args.get("query")
-        result = sqliteGet("SELECT username, private FROM users WHERE username LIKE ?", ("%" + query + "%",))
+        result = sqlite_get("SELECT username, private FROM users WHERE username LIKE ?", ("%" + query + "%",))
         names = [user[0] for user in result if not user[1] and user[0] != session["user"]]
         return render_template("search.html", names=names, query=query)
     for user, group in request.json.items():
-        result = ast.literal_eval(sqliteGet("SELECT pending FROM groups WHERE name = ?", (group,))[0][0])
+        result = literal_eval(sqlite_get("SELECT pending FROM groups WHERE name = ?", (group,))[0][0])
         if (any(i[1] == user for i in result)):
             pending = [[i[0] + [session["user"]], i[1]] if session["user"] not in i[0] and i[1] == user else i for i in result]
         else:
             pending = result + [[[session["user"]], user]]
-        sqliteQuery("UPDATE groups SET pending = ? WHERE name = ?", (repr(pending), group))
+        sqlite_execute("UPDATE groups SET pending = ? WHERE name = ?", (repr(pending), group))
     return json.dumps({"status": 0})
 
 
-@app.route("/group", methods=["GET", "POST"])
+@app.route("/groups", methods=["GET", "POST"])
 @signin_required(True)
 @search_logout
-def group():
+def groups():
+    # GET
     if request.method == "GET":
-        result = sqliteGet("SELECT name, pending FROM groups WHERE pending LIKE ?", ("%'" + session["user"] + "'%",))
-        invitations = [[[i for i in ast.literal_eval(p) if i[1] == session["user"]][0][0], n] for n, p in result]
-        return render_template("group.html", invitations=invitations)
+        pendings = sqlite_get("SELECT name, pending FROM groups WHERE pending LIKE ?", ("%'" + session["user"] + "'%",))
+        invitations = []
+        for n, p in pendings:
+            try:
+                invitors = [i for i in literal_eval(p) if i[1] == session["user"]][0][0]
+            except IndexError:
+                continue
+            invitations.append((invitors, n))
+        return render_template("groups.html", groups=get_groups(), invitations=invitations)
+
+    # Create or join group
     if request.method == "POST" and request.is_json:
-        result = sqliteGet("SELECT * FROM groups WHERE name = ?", (request.json["name"],))
+        result = sqlite_get("SELECT password, members FROM groups WHERE name = ?", (request.json["name"],))
         if not result:
-            return json.dumps({"status": 1})
-        members = ast.literal_eval(result[0][2])
+            if "confirm" not in request.json:
+                return json.dumps({"status": 1})  # Confirm create
+            # Create group
+            sqlite_execute("INSERT INTO groups (name, password, members) VALUES (?, ?, ?)", (request.json["name"], crypt.crypt(request.json["password"], crypt.METHOD_SHA512), repr([session["user"]])))
+            return json.dumps({"status": 0})
+        # Join group
+        members = literal_eval(result[0][1])
         if session["user"] in members:
-            return json.dumps({"status": 2})
-        if result[0][1] != request.json["password"]:
-            return json.dumps({"status": 3})
+            return json.dumps({"status": 2})  # Already a member
+        if not compare_digest(crypt.crypt(request.json["password"], result[0][0]), result[0][0]):
+            return json.dumps({"status": 3})  # Incorrect password
         members.append(session["user"])
-        sqliteQuery("UPDATE groups SET members = ? WHERE name = ?", (repr(members), result[0][0]))
-        session["groups"].append((result[0][0], members))
-        session.modified = True
-        return json.dumps({"status": 0})
+        sqlite_execute("UPDATE groups SET members = ? WHERE name = ?", (repr(members), request.json["name"]))
+        return json.dumps({"status": 0, "groups": get_groups()})
+
+    # Leave group
     if request.method == "POST" and "leave" in request.form:
-        members = ast.literal_eval(sqliteGet("SELECT members FROM groups WHERE name = ?", (request.form["leave"],))[0][0])
+        members = literal_eval(sqlite_get("SELECT members FROM groups WHERE name = ?", (request.form["leave"],))[0][0])
         members.remove(session["user"])
-        sqliteQuery("UPDATE groups SET members = ? WHERE name = ?", (repr(members), request.form["leave"]))
-        session["groups"] = [i for i in session["groups"] if i[0] != request.form["leave"]]
-        session.modified = True
-        return redirect(url_for("group"))
+        sqlite_execute("UPDATE groups SET members = ? WHERE name = ?", (repr(members), request.form["leave"]))
+        return redirect(url_for("groups"))
         # What if the group has no members left?
+
+    # Accept or reject invitations
     form = request.form.get("accept") or request.form.get("reject")
-    result = ast.literal_eval(sqliteGet("SELECT pending FROM groups WHERE name = ?", (form,))[0][0])
+    result = literal_eval(sqlite_get("SELECT pending FROM groups WHERE name = ?", (form,))[0][0])
     pending = [i for i in result if i[1] != session["user"]]
-    sqliteQuery("UPDATE groups SET pending = ? WHERE name = ?", (repr(pending), form))
+    sqlite_execute("UPDATE groups SET pending = ? WHERE name = ?", (repr(pending), form))
     if "accept" in request.form:
-        members = ast.literal_eval(sqliteGet("SELECT members FROM groups WHERE name = ?", (request.form["accept"],))[0][0])
+        members = literal_eval(sqlite_get("SELECT members FROM groups WHERE name = ?", (form,))[0][0])
         members.append(session["user"])
-        sqliteQuery("UPDATE groups SET members = ? WHERE name = ?", (repr(members), request.form["accept"]))
-        session["groups"].append((request.form["accept"], members))
-        session.modified = True
-    return redirect(url_for("group"))
+        sqlite_execute("UPDATE groups SET members = ? WHERE name = ?", (repr(members), form))
+    return redirect(url_for("groups"))
 
 
-@app.route("/group/create", methods=["POST"])
-def group_create():
-    sqliteQuery("INSERT INTO groups (name, password, members) VALUES (?, ?, ?)", (request.json["name"], request.json["password"], repr([session["user"]])))
-    session["groups"].append((request.json["name"], [session["user"]]))
-    session.modified = True
-    return json.dumps({"status": 0})
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+app.run(debug=True, host="0.0.0.0")
